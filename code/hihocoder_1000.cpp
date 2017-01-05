b72a3fb5e1429f711074179af692433f9b1793d6
bjzhang003 <bjzhang03@outlook>
2017-01-05 15:39:31 +0800

    add 1000

---
+++ code/hihocoder_1000.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+    > File Name: hihocoder_1000.cpp
+    > Author: bjzhang03
+    > Mail: bjzhang03@outlook.com
+    > Created Time: 2017年01月05日 星期四 15时36分49秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=0,b=0;
+	while(cin>>a>>b)
+		cout<<a+b<<endl;
+	return 0;
+}
