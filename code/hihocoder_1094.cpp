7c1515396696af1b10b371b8b41d267d97778562
bjzhang003 <bjzhang03@outlook>
2017-01-05 21:39:41 +0800

    update README

---
+++ code/hihocoder_1094.cpp
@@ -0,0 +1,10 @@
+/*************************************************************************
+    > File Name: hihocoder_1094.cpp
+    > Author: bjzhang03
+    > Mail: bjzhang1991@gmail.com
+    > Created Time: 2017年01月05日 星期四 21时35分16秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
